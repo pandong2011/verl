9d889dac203e24cb7d3bb399defa8309e243b302
pandong2011 <panqw2011@163.com>
2025-08-03 01:17:23 +0800

    add execute install_req script

---
+++ install_req.sh
@@ -0,0 +1,8 @@
+# 依赖所需的连续依赖会自动安装
+# 激活环境
+# source venv/bin/activate
+# 确认激活环境： python -c "import sys; print(sys.prefix)"
+# 安装依赖：⚠️安装成功后才会在对应的地方显示依赖
+# pip install -r requirements.txt
+# pip install -i https://pypi.mirrors.ustc.edu.cn/simple -r requirements.txt
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
